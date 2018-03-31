35b8370bf141e5b81247e92d41e3da8aa3567a02
Sergey <serkov23@mail.ru>
2018-03-31 17:03:41 +0300

    Initial commit
    Signed-off-by: Sergey <serkov23@mail.ru>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <windows.h>
+#include "MessageMap.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+class Program{
+    static WinApi::MessageMap map;
+    static HINSTANCE hinst;
+public:
+    static int main(HINSTANCE hInstance,int nCmdShow){
+        hinst=hInstance;
+
+    }
+};
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
     return 0;
 }
